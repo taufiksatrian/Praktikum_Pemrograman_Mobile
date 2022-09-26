b22bab0ba5be1892d99dc24723e7d2a9a091ac92
taufiksatrian <taufiksatrianugraha21@gmail.com>
2022-09-26 09:41:05 +0700

    Tugas_3 Taufik Satria Nugraha_H1D020028_SHIFT G

---
+++ app/src/main/java/com/example/praktikum_pemrograman_mobile/success_login.java
@@ -0,0 +1,14 @@
+package com.example.praktikum_pemrograman_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class success_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success_login);
+    }
+}
